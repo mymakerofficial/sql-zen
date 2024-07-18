60492ed72e3951cd0e568a373f744cebff0aebb1
My_Maker <36822242+mymakerofficial@users.noreply.github.com>
2024-07-18 11:35:05 +0200

    chore: add shiki

--- src/components/Editor.vue
+++ src/components/Editor.vue
@@ -1,19 +1,38 @@
 <script setup lang="ts">
 import * as monaco from 'monaco-editor'
+import { createHighlighter, type HighlighterGeneric } from 'shiki'
+import { shikiToMonaco } from '@shikijs/monaco'
 import { onMounted, onScopeDispose, ref } from 'vue'
 
 const container = ref<HTMLElement | null>(null)
 const editor = ref<monaco.editor.IStandaloneCodeEditor | null>(null)
+const highlighter = ref<HighlighterGeneric<any, any> | null>(null)
 
-onMounted(() => {
+onMounted(async () => {
+  highlighter.value = await createHighlighter({
+    themes: [
+      'vitesse-dark',
+      'vitesse-light',
+    ],
+    langs: [
+      'sql'
+    ],
+  })
+
+  monaco.languages.register({ id: 'sql' })
+
+  shikiToMonaco(highlighter.value, monaco)
+
   editor.value = monaco.editor.create(container.value!, {
     value: 'select * from users;',
     language: 'sql',
+    theme: 'vitesse-dark',
   })
 })
 
 onScopeDispose(() => {
   editor.value?.dispose()
+  highlighter.value?.dispose()
 })
 </script>
 
