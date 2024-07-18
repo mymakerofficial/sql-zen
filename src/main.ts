beea4fa0d968dbad3683320ac6d6e1e6182c0d36
My_Maker <36822242+mymakerofficial@users.noreply.github.com>
2024-07-18 10:55:23 +0200

    chore: add unplugin-vue-router

--- src/main.ts
+++ src/main.ts
@@ -2,9 +2,19 @@ import './assets/main.css'
 
 import { createApp } from 'vue'
 import App from './App.vue'
-import router from './router'
+import { createRouter, createWebHistory } from 'vue-router'
+import { routes, handleHotUpdate } from 'vue-router/auto-routes'
 
 const app = createApp(App)
+
+const router = createRouter({
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes,
+})
+
+if (import.meta.hot) {
+  handleHotUpdate(router)
+}
 
 app.use(router)
 
--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,14 @@
 import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig } from 'vite'
+import vueRouter from 'unplugin-vue-router/vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
+    vueRouter(),
     vue(),
     vueDevTools(),
   ],
