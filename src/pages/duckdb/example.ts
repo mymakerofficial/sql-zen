ac548296dbfda4abeec8db3228d9579d5c97e8d5
My_Maker <36822242+mymakerofficial@users.noreply.github.com>
2024-07-19 21:45:51 +0200

    fix: was not using https

--- src/pages/duckdb/example.ts
+++ src/pages/duckdb/example.ts
@@ -1,10 +1,10 @@
-const host = window.location.host;
+const origin = window.location.origin;
 
 export default `-- Welcome to sql-zen
 -- You can run queries by clicking the "Run All" button
 
 SELECT station_name, count(*) AS num_services
-FROM 'http://${host}/train_services.parquet'
+FROM '${origin}/train_services.parquet'
 GROUP BY ALL
 ORDER BY num_services DESC
 LIMIT 10;`
