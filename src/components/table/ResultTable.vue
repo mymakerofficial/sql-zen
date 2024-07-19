e2178944b1597430ac61afaf74f67f3d48049031
My_Maker <36822242+mymakerofficial@users.noreply.github.com>
2024-07-19 12:07:29 +0200

    feat: added ResultTable component

---
+++ src/components/table/ResultTable.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+import DataTable from '@/components/table/DataTable.vue'
+import { createColumnHelper } from '@tanstack/vue-table'
+import { computed } from 'vue'
+
+const props = defineProps<{
+  data: Array<object>
+}>()
+
+const columnHelper = createColumnHelper<object>()
+const columns = computed(() => {
+  return props.data.length
+    ? Object.keys(props.data[0]).map((key) =>
+      // @ts-ignore // TODO: Fix this
+        columnHelper.accessor(key, {
+          header: key,
+        }),
+      )
+    : []
+})
+</script>
+
+<template>
+  <DataTable :data="data" :columns="columns" />
+</template>
