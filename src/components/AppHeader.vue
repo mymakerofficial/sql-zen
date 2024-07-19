9cddd64b65fd555bc28ec057b6eeff7d02524a99
My_Maker <36822242+mymakerofficial@users.noreply.github.com>
2024-07-19 11:05:21 +0200

    feat: added descriptions for databases

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -7,8 +7,6 @@ const database = defineModel<string>('database', { default: 'postgresql' })
 <template>
   <nav class="relative h-16 px-3 flex items-center border-b border-border">
     <span class="w-96 h-full absolute left-0 -z-10 bg-gradient-to-r from-blue-500/20 to-blue-500/0" />
-    <div class="w-60">
-      <DatabaseSelect v-model="database" />
-    </div>
+    <DatabaseSelect v-model="database" />
   </nav>
 </template>
--- src/components/DatabaseSelect.vue
+++ src/components/DatabaseSelect.vue
@@ -1,26 +1,32 @@
 <script setup lang="ts">
-import { Select, SelectTrigger, SelectValue, SelectContent, SelectGroup, SelectItem } from '@/components/ui/select'
+import { Select, SelectContent, SelectGroup, SelectItem, SelectTrigger, SelectValue } from '@/components/ui/select'
 
 const model = defineModel<string>({ default: 'postgresql' })
 </script>
 
 <template>
   <Select v-model="model">
-    <SelectTrigger class="bg-transparent border-transparent">
-      <SelectValue placeholder="Select database..." />
+    <SelectTrigger class="w-fit bg-transparent border-transparent [&_[data-description]]:hidden">
+      <SelectValue placeholder="Select database..." class="pr-3" />
     </SelectTrigger>
     <SelectContent>
       <SelectGroup>
-        <SelectItem value="postgresql">
-          <div class="flex">
-            <img src="@/assets/icons/postgresql.svg" alt="PostgreSQL" class="size-6 mr-2" />
-            <span>PostgreSQL</span>
+        <SelectItem value="postgresql" class="px-3">
+          <div class="my-1 flex flex-col gap-2 max-w-80">
+            <div class="flex items-center gap-2">
+              <img src="@/assets/icons/postgresql.svg" alt="PostgreSQL" class="size-6" />
+              <span>PostgreSQL</span>
+            </div>
+            <p class="pl-8 pr-2 text-xs text-muted-foreground" data-description>PostgreSQL is a versatile database offering advanced features like JSON support, full-text search, and powerful analytics capabilities.</p>
           </div>
         </SelectItem>
-        <SelectItem value="sqlite" class="flex">
-          <div class="flex">
-            <img src="@/assets/icons/sqlite.svg" alt="SQLite" class="size-6 mr-2" />
-            <span>SQLite</span>
+        <SelectItem value="sqlite" class="px-3">
+          <div class="my-1 flex flex-col gap-2 max-w-80">
+            <div class="flex items-center gap-2">
+              <img src="@/assets/icons/sqlite.svg" alt="SQLite" class="size-6" />
+              <span>SQLite</span>
+            </div>
+            <p class="pl-8 pr-2 text-xs text-muted-foreground" data-description>SQLite is a lightweight, embedded database ideal for small-scale applications, offering simplicity, speed, and zero configuration.</p>
           </div>
         </SelectItem>
       </SelectGroup>
