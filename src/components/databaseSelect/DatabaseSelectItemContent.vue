a593b54b9ee7a54231e1b40825d2c4b0d5455be4
My_Maker <36822242+mymakerofficial@users.noreply.github.com>
2024-07-20 20:27:50 +0200

    feat: one pixel makes the difference

--- src/components/databaseSelect/DatabaseSelectItemContent.vue
+++ src/components/databaseSelect/DatabaseSelectItemContent.vue
@@ -6,11 +6,11 @@ defineProps<DatabaseSystemInfo>()
 
 <template>
   <div class="my-1 flex flex-col gap-2 max-w-80">
-    <div class="flex items-center gap-2">
+    <div class="flex items-center gap-3">
       <img :src="icon" :alt="`${name} Icon`" class="size-6" />
       <span>{{ name }}</span>
     </div>
-    <p class="pl-8 pr-2 text-xs text-muted-foreground" data-description>
+    <p class="pl-9 pr-2 text-xs text-muted-foreground" data-description>
       {{ description }}
     </p>
   </div>
