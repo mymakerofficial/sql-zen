14fe85f7efd75428f3bb26049cff2a2ab797b3a5
My_Maker <36822242+mymakerofficial@users.noreply.github.com>
2024-07-20 21:27:32 +0200

    "fix" type

--- src/composables/useActualColorMode.ts
+++ src/composables/useActualColorMode.ts
@@ -4,5 +4,6 @@ import { computed } from 'vue'
 export function useActualColorMode() {
   const { system, state } = useColorMode()
 
+  // @ts-ignore for some reason, 'system' is not defined in the type definition
   return computed(() => (state.value === 'system' ? system.value : state.value))
 }
