a60180e0a56a3e8b281258ec87994dbf499ec641
My_Maker <36822242+mymakerofficial@users.noreply.github.com>
2024-07-21 15:12:48 +0200

    feat: scroll to bottom

--- src/components/logger/LoggerList.vue
+++ src/components/logger/LoggerList.vue
@@ -2,21 +2,34 @@
 import type { Logger } from '@/lib/logger/logger'
 import LogItem from '@/components/logger/LogItem.vue'
 import { useLoggerEvents } from '@/composables/useLoggerEvents'
+import { nextTick, ref, watch } from 'vue'
+import { whenever } from '@vueuse/core'
 
 const props = defineProps<{
   logger: Logger
 }>()
 
+const container = ref<HTMLElement | null>(null)
 const events = useLoggerEvents(props.logger)
+
+async function scrollToBottom() {
+  if (!container.value) {
+    return
+  }
+  await nextTick()
+  await nextTick()
+  await nextTick()
+  container.value.scrollTo({
+    top: container.value.scrollHeight,
+  })
+}
+
+watch(events, scrollToBottom)
+whenever(container, scrollToBottom)
 </script>
 
 <template>
-  <div class="flex-1 overflow-y-auto">
-    <div v-if="true" class="flex flex-col">
-      <LogItem v-for="event in events" :key="event.key" :event="event" />
-    </div>
-    <div v-else class="h-24 flex items-center justify-center">
-      <i class="text-muted-foreground">*crickets*</i>
-    </div>
+  <div ref="container" class="h-full flex flex-col overflow-y-auto">
+    <LogItem v-for="event in events" :key="event.key" :event="event" />
   </div>
 </template>
