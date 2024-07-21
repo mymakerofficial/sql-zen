1b43767f0c19fb64eb9576f55a51e769305f8743
My_Maker <36822242+mymakerofficial@users.noreply.github.com>
2024-07-21 16:08:29 +0200

    feat: added xs size for toggle

--- src/components/ui/toggle/index.ts
+++ src/components/ui/toggle/index.ts
@@ -13,6 +13,7 @@ export const toggleVariants = cva(
       },
       size: {
         default: 'h-10 px-3',
+        xs: 'h-7 rounded px-2',
         sm: 'h-9 px-2.5',
         lg: 'h-11 px-5',
       },
