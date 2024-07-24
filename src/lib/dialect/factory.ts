d8dd207985c3dbab4e888ce2b17a6d456c36d9fd
My_Maker <36822242+mymakerofficial@users.noreply.github.com>
2024-07-24 19:57:59 +0200

    feat: get tables for sqlite

--- src/lib/dialect/factory.ts
+++ src/lib/dialect/factory.ts
@@ -3,6 +3,7 @@ import type { DataSourceFacade } from '@/lib/databases/database'
 import { DatabaseEngine } from '@/lib/databaseEngines'
 import { DummyDialect } from '@/lib/dialect/dialect'
 import { PostgreSQLDialect } from '@/lib/dialect/postgresql'
+import { SQLiteDialect } from '@/lib/dialect/sqlite'
 
 export class SqlDialectFactory {
   static create(dataSource: DataSourceFacade) {
@@ -10,6 +11,8 @@ export class SqlDialectFactory {
       return new DuckDBDialect(dataSource)
     } else if (dataSource.engine === DatabaseEngine.PostgreSQL) {
       return new PostgreSQLDialect(dataSource)
+    } else if (dataSource.engine === DatabaseEngine.SQLite) {
+      return new SQLiteDialect(dataSource)
     } else {
       return new DummyDialect(dataSource)
     }
