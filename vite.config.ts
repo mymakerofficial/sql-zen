e8ad79dd6db65942116d865edfa2d9266dbc5364
My_Maker <36822242+mymakerofficial@users.noreply.github.com>
2024-08-12 20:30:57 +0200

    upgrade to tauri 2

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,8 @@ import vueRouter from 'unplugin-vue-router/vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
 
+const host = process.env.TAURI_DEV_HOST
+
 // https://vitejs.dev/config/
 export default defineConfig({
   optimizeDeps: {
@@ -30,6 +32,14 @@ export default defineConfig({
   server: {
     port: 1420,
     strictPort: true,
+    host: host || false,
+    hmr: host
+      ? {
+          protocol: 'ws',
+          host,
+          port: 1421,
+        }
+      : undefined,
     watch: {
       // tell vite to ignore watching `src-tauri`
       ignored: ['**/src-tauri/**'],
