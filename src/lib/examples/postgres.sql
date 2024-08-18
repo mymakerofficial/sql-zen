fba3a3fad98bcce381c94ca2e9303835451cd63d
My_Maker <36822242+mymakerofficial@users.noreply.github.com>
2024-08-18 21:16:05 +0200

    feat: update postgres example

--- src/lib/examples/postgres.sql
+++ src/lib/examples/postgres.sql
@@ -1,34 +1,27 @@
--- Load pgvector extension
-CREATE EXTENSION IF NOT EXISTS vector;
-
 -- Create a table to store products
 CREATE TABLE products (
     id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
     name TEXT NOT NULL UNIQUE,
-    data JSON,
-    price NUMERIC,
+    heading TEXT,
+    description TEXT,
+    price MONEY,
     weight NUMERIC,
-    rating NUMERIC,
-    embedding VECTOR(3)
+    rating NUMERIC
 );
 
 -- Insert some data
-INSERT INTO products (name, data, price, weight, rating, embedding)
+INSERT INTO products (name, heading, description, price, weight, rating)
 VALUES
-    ('Quantum Leap Headphones', '{"heading": "Sonic Bliss Unleashed", "description": "Immersive audio with noise cancellation that transports you."}', 299.99, 0.5, 4.5, '[0.1, 0.9, 0.2]'),
-    ('Smartphone', '{"heading": "Your Digital Life", "description": "The ultimate device for staying connected and entertained."}', 999.99, 0.3, 4.7, '[0.2, 0.8, 0.1]'),
-    ('Smartwatch', '{"heading": "Your Wrist, Your World", "description": "Stay connected and track your fitness goals."}', 199.99, 0.2, 4.0, '[0.3, 0.7, 0.3]'),
-    ('Office Chair', '{"heading": "Elevate Your Workspace", "description": "Experience optimal comfort and support while working."}', 1499.99, 10.0, 4.8, '[0.4, 0.6, 0.4]'),
-    ('Virtual Reality Headset', '{"heading": "Escape the Ordinary", "description": "Your new favorite way to avoid chores."}', 399.99, 1.0, 4.2, '[0.5, 0.5, 0.5]');
+    ('Quantum Leap Headphones', 'Sonic Bliss Unleashed', 'Immersive audio with noise cancellation that transports you.', 299.99, 0.5, 4.5),
+    ('Smartphone', 'Your Digital Life', 'The ultimate device for staying connected and entertained.', 999.99, 0.3, 4.7),
+    ('Smartwatch', 'Your Wrist, Your World', 'Stay connected and track your fitness goals.', 199.99, 0.2, 4.0),
+    ('Office Chair', 'Elevate Your Workspace', 'Experience optimal comfort and support while working.', 1499.99, 0.0, 4.8),
+    ('Virtual Reality Headset', 'Escape the Ordinary', 'Your new favorite way to avoid chores.', 399.99, 1.0, 4.2);
 
--- Select all products, ordered by similarity to the 'Virtual Reality Headset'
+-- Select all products
 SELECT
     name,
-    data->>'heading' AS heading,
-    data->>'description' AS description,
-    price,
-    weight,
-    rating,
-    embedding <-> (SELECT embedding FROM products WHERE name LIKE '%Headset%') AS distance
+    concat(heading, ': ', description) as details,
+    price
 FROM products
-ORDER BY distance;
+ORDER BY price DESC;
