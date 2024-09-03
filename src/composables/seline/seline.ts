6862d92576abbaec92d98ef4f72bac4c3895564f
My_Maker <36822242+mymakerofficial@users.noreply.github.com>
2024-09-03 16:59:48 +0200

    feat: remove custom user id

--- src/composables/seline/seline.ts
+++ src/composables/seline/seline.ts
@@ -1,20 +1,5 @@
 import * as seline from '@seline-analytics/web'
-import { createGlobalState, useLocalStorage } from '@vueuse/core'
-import { getId } from '@/lib/getId'
 
-const useAnalyticsUser = createGlobalState(() => {
-  // generate a random id **not based on any device data**
-  const userId = useLocalStorage('random-userid', getId('user'))
-
-  return {
-    userId: userId.value,
-    name: userId.value,
-  }
-})
-
-export function initSeline()  {
-  const user = useAnalyticsUser()
-  console.debug("seline user:", user)
-  seline.init();
-  seline.setUser(user)
+export function initSeline() {
+  seline.init()
 }
