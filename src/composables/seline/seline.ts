a84e8932ffaa9924a61349ee9a51a02bd3ffde7a
My_Maker <36822242+mymakerofficial@users.noreply.github.com>
2024-09-04 15:53:40 +0200

    feat: include git commit in analytics user data

--- src/composables/seline/seline.ts
+++ src/composables/seline/seline.ts
@@ -75,7 +75,15 @@ function initSeline() {
     return
   }
 
-  seline.init()
+  seline.init({
+    autoPageView: false,
+  })
+  seline.setUser({
+    gitBranch: import.meta.env.VERCEL_GIT_COMMIT_REF || 'unknown',
+    gitCommitSha: import.meta.env.VERCEL_GIT_COMMIT_SHA || 'unknown',
+  })
+  // to prevent automatic page view tracking before user data is set
+  seline.enableAutoPageView()
 
   log('Thank you for helping us improve SqlZen! 🤗')
 }
