2f49f70148ae6a4bf50f6f46d5fedfe9c516af48
My_Maker <36822242+mymakerofficial@users.noreply.github.com>
2024-09-09 08:45:00 +0200

    feat: remove queries: single-completed event

--- src/lib/runner/plugins/analytics.ts
+++ src/lib/runner/plugins/analytics.ts
@@ -6,26 +6,16 @@ import {
   isErrorQuery,
   isSuccessQuery,
 } from '@/lib/queries/helpers'
-import type { QueryInfo } from '@/lib/queries/interface'
 
 const { track } = useSeline()
 
 export function runnerAnalytics(runner: Runner) {
-  function trackSingleQuery(query: QueryInfo) {
-    const { state, hasResultRows } = query
-
-    track('queries: single-completed', {
-      ...runner.dataSource.getAnonymizedAnalyticsData(),
-      state,
-      hasResultRows,
-    })
-  }
-
-  function trackBatchQuery(queries: QueryInfo[], transacting: boolean) {
+  runner.on(RunnerEvent.BatchCompleted, (queries, transacting) => {
     const queryCount = queries.length
     const successCount = queries.filter(isSuccessQuery).length
     const errorCount = queries.filter(isErrorQuery).length
     const hasResultRowsCount = queries.filter(hasResultRows).length
+    const hasCommentCount = queries.filter((query) => !!query.statement.comment).length
 
     track('queries: batch-completed', {
       ...runner.dataSource.getAnonymizedAnalyticsData(),
@@ -33,15 +23,8 @@ export function runnerAnalytics(runner: Runner) {
       successCount,
       errorCount,
       hasResultRowsCount,
+      hasCommentCount,
       transacting,
     })
-  }
-
-  runner.on(RunnerEvent.BatchCompleted, (queries, transacting) => {
-    if (queries.length === 1) {
-      trackSingleQuery(queries[0])
-    } else {
-      trackBatchQuery(queries, transacting)
-    }
   })
 }
