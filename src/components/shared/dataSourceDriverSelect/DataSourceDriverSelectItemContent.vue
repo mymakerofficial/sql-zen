975171346d04e68146f5fe7f9255edfd9ec74f2b
My_Maker <36822242+mymakerofficial@users.noreply.github.com>
2024-10-05 11:01:59 +0200

    feat: show requires desktop in select

--- src/components/shared/dataSourceDriverSelect/DataSourceDriverSelectItemContent.vue
+++ src/components/shared/dataSourceDriverSelect/DataSourceDriverSelectItemContent.vue
@@ -1,7 +1,21 @@
 <script setup lang="ts">
 import type { DataSourceDriverInfo } from '@/lib/engines/interface'
+import { useDriverSupports } from '@/composables/engines/useDriverSupports'
+import { DataSourceDriverCapability } from '@/lib/engines/enums'
+import { AppWindowIcon } from 'lucide-vue-next'
+import { computed } from 'vue'
+import { isTauri } from '@tauri-apps/api/core'
 
-defineProps<DataSourceDriverInfo>()
+const props = defineProps<DataSourceDriverInfo>()
+
+const requiresDesktop = useDriverSupports(
+  () => props.driver,
+  DataSourceDriverCapability.RequiresDesktopApp,
+)
+
+const showRequiresDesktop = computed(() => {
+  return requiresDesktop.value && !isTauri()
+})
 </script>
 
 <template>
@@ -9,6 +23,14 @@ defineProps<DataSourceDriverInfo>()
     <div class="flex items-center gap-3">
       <img :src="icon" :alt="`${name} Icon`" class="size-4 min-h-max" />
       <span>{{ name }}</span>
+      <span
+        v-if="showRequiresDesktop"
+        class="flex gap-1 items-center text-xs px-1 py-0.5 rounded-sm bg-rose-500/20 text-rose-200"
+        data-description
+      >
+        <AppWindowIcon class="size-3" />
+        Requires Desktop App
+      </span>
     </div>
     <p class="pl-7 pr-2 text-xs text-muted-foreground" data-description>
       {{ description }}
