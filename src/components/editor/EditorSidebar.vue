3850b3b0b023b6fc87582eeafa0c1f88a13869ab
My_Maker <36822242+mymakerofficial@users.noreply.github.com>
2024-10-06 20:13:00 +0200

    fix: editor sidebar logo

--- src/components/editor/EditorSidebar.vue
+++ src/components/editor/EditorSidebar.vue
@@ -13,8 +13,10 @@ import { Button } from '@/components/ui/button'
 import DatabaseExplorerContent from '@/components/databaseExplorer/DatabaseExplorerContent.vue'
 import { useEnv } from '@/composables/useEnv'
 import { cn } from '@/lib/utils'
+import AppLogo from '@/components/shared/appHeader/AppLogo.vue'
+import { PlusIcon } from 'lucide-vue-next'
 
-const { isMacOS } = useEnv()
+const { isMacOS, isTauri } = useEnv()
 const { open: openCreate } = useDialog(CreateDataSourceDialog)
 </script>
 
@@ -23,15 +25,21 @@ const { open: openCreate } = useDialog(CreateDataSourceDialog)
     <SheetTrigger><slot /></SheetTrigger>
     <SheetContent side="left" class="h-full flex flex-col">
       <SheetHeader v-if="!isMacOS" class="text-left">
-        <SheetTitle><LogoButton /></SheetTitle>
+        <SheetTitle>
+          <LogoButton v-if="!isTauri" />
+          <AppLogo v-else class="ml-3" />
+        </SheetTitle>
       </SheetHeader>
       <div
         :class="
-          cn('-mx-6 -mb-6 px-6 py-3 overflow-y-auto', isMacOS ? 'pt-6' : '')
+          cn('-mx-6 -mb-6 px-6 py-3 overflow-y-auto', isMacOS ? 'pt-7' : '')
         "
       >
         <div class="h-min flex flex-col gap-4">
-          <Button @click="openCreate"> Add Data Source </Button>
+          <Button @click="openCreate" variant="secondary" class="gap-2">
+            <PlusIcon class="size-4 min-h-max" />
+            <span>Add Data Source</span>
+          </Button>
           <DatabaseExplorerContent class="pl-3 w-full h-min" />
         </div>
       </div>
--- src/components/shared/appHeader/AppLogo.vue
+++ src/components/shared/appHeader/AppLogo.vue
@@ -3,7 +3,7 @@
     <img
       src="@/assets/sql-zen-logo.svg"
       alt="SqlZen"
-      class="inline size-6 min-h-max mr-3"
+      class="inline size-6 mr-3"
     />
     <span
       class="inline-block text-xl bg-gradient-to-br from-blue-200 to-blue-600 text-transparent bg-clip-text font-black"
