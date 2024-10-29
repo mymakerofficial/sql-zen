73ac6f35a19ce2c11b38ef6683e192ff1a29c71b
My_Maker <36822242+mymakerofficial@users.noreply.github.com>
2024-10-29 18:25:31 +0100

    chore: add process plugin

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -159,6 +159,7 @@ fn setup_window(app: &mut tauri::App) -> Result<(), tauri::Error> {
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder::default()
+        .plugin(tauri_plugin_process::init())
         .plugin(tauri_plugin_updater::Builder::new().build())
         .plugin(tauri_plugin_os::init())
         .plugin(tauri_plugin_shell::init())
