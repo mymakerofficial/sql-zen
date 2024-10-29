46b7b1edf2a5ba9442f98d107b0d5f11a2bd6989
My_Maker <36822242+mymakerofficial@users.noreply.github.com>
2024-10-29 18:20:58 +0100

    fix: bumpp didnt update Cargo.lock

--- bump.config.ts
+++ bump.config.ts
@@ -5,6 +5,10 @@ export default defineConfig({
     'package.json',
     'package-lock.json',
     './src-tauri/Cargo.toml',
+    './src-tauri/Cargo.lock',
     './src-tauri/tauri.conf.json',
   ],
+  // update the lockfile because bumpp can't properly update it
+  execute:
+    'cargo generate-lockfile --offline --quiet --manifest-path ./src-tauri/Cargo.toml',
 })
