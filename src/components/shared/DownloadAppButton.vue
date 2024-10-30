3d8e2ca1077de2098e35444666552bb9f515249a
My_Maker <36822242+mymakerofficial@users.noreply.github.com>
2024-10-30 19:09:45 +0100

    feat: add download app button to browser app header

---
+++ src/components/shared/DownloadAppButton.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+import { DownloadIcon } from 'lucide-vue-next'
+import { Button } from '@/components/ui/button'
+import { useEnv } from '@/composables/useEnv'
+
+const { isBrowser } = useEnv()
+</script>
+
+<template>
+  <Button
+    as-child
+    v-if="isBrowser"
+    variant="ghost"
+    class="gap-3"
+  >
+    <RouterLink to="/download">
+      <DownloadIcon class="size-4" />
+      <span>Download App</span>
+    </RouterLink>
+  </Button>
+</template>
