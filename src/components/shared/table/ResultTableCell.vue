6eb95a6b0236e09f7fb236de72f46d86bdeb2827
My_Maker <36822242+mymakerofficial@users.noreply.github.com>
2024-11-19 13:29:50 +0100

    feat: highlight null or empty values

---
+++ src/components/shared/table/ResultTableCell.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+import type { FieldInfo } from '@/lib/schema/columns/column'
+import { computed } from 'vue'
+
+const props = defineProps<{
+  field: FieldInfo
+  value: any
+}>()
+
+const isNull = computed(() => props.value === null)
+const isUndefined = computed(() => props.value === undefined)
+const isEmpty = computed(() => props.value === '')
+</script>
+
+<template>
+  <span v-if="isNull" class="text-muted-foreground italic">NULL</span>
+  <span v-else-if="isUndefined" class="text-muted-foreground italic">UNDEFINED</span>
+  <span v-else-if="isEmpty" class="text-muted-foreground italic">EMPTY</span>
+  <span v-else>{{ value }}</span>
+</template>
